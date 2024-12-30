4082b9c02d405847c4e79f9c3920887f42c52cf8
ro <54208873+rohitp934@users.noreply.github.com>
2024-12-30 12:51:27 -0500

    Basic formula, will update soon

---
+++ Formula/glimpse.rb
@@ -0,0 +1,17 @@
+class Glimpse < Formula
+  desc "Blazingly fast tool for peeking at codebases"
+  homepage "https://github.com/seatedro/glimpse"
+  url "https://github.com/seatedro/glimpse/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "HASH"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/glimpse", "--version"
+  end
+end
